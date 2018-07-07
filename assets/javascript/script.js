896c2936efed7feb581cfbbb1528c614b61ee2dc
JLasc <lascaze.j@gmail.com>
2018-07-07 15:37:36 -0400

    added psuedo code, created global var, wrote out empty func for game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,83 @@
-var butter = "1";
+/* 
+Psuedo Code:
+
+- Game initializes upon page load - function that resets totals and chooses new values
+- Computer will pick a random target number using math.random 30-50 (keep this range, and increment on new game)
+- User will be shown 4 crystals, and each has a hidden value to the user (array with 4 random numbers, use for loop to assign img & value)
+- If the user can match the target number, then win, else loss. Loss is if user goes over target number.
+- Game resets on win/loss 
+
+
+** Extras ** 
+
+Sound for win & loss
+Sound for crystal click
+Game loss banner display? 
+Game win banner?
+
+*/
+
+
+
+
+
+/* $(document).ready(function() { */
+
+//Global Variables
+var crystalArray = [];
+var userNumber = 0;
+var targetNumber = 0;
+var wins = 0;
+var losses = 0;
+var gameOver = true;
+
+//Page hooks
+var tarNum = $("#target-number");
+var cryDiv = $("#crystal-div");
+var userNum = $("#user-number");
+
+
+//Start the game
+function gameStart () {
+    gameOver = false;
+    userNumber = 0;
+    getTargetNumber()
+};
+
+//Fill crystalArray with 4 values & generates image on page
+function crystalFill () {
+
+    //Empties array
+    crystalArray = [];
+
+    // Adds 4 random numbers between 1 & 10
+    for (i = 1; i <= 4; i++ ) {
+        a = Math.floor(Math.random() * 10) +1
+        crystalArray.push(a)
+    }
+
+    //Creates image, and add values
+    for (j = 0; j < crystalArray.length; j++) {
+        a = $("<img>")
+        a.addClass("crystal-size")
+        a.attr("src", "./assets/images/crystalgif.gif")
+        a.attr("crystal-values", crystalArray[j])
+        
+    }
+
+
+};
+
+// Generates a target number between 30-50, increments upon win
+function getTargetNumber () {
+
+}
+
+
+//Game Logic
+
+
+//Initialize game on page load
+
+
+/* }) */
